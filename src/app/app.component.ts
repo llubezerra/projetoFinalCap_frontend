0ff86179dd368d139dfa25c32e74eeafaf956732
Luana Bezerra <lu.oliveira1994@hotmail.com>
2021-11-17 15:06:58 -0300

    inicio estilizacao aplicacao nova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-exemplo';
+  title = 'Aplicação exemplo de front e back';
 }
