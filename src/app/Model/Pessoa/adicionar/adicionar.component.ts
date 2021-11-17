52fbc2436be53a357393f663a69a17a1e35a7a97
Luana Bezerra <lu.oliveira1994@hotmail.com>
2021-11-17 15:57:31 -0300

    add listar/adicionar/editar

---
+++ src/app/Model/Pessoa/adicionar/adicionar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar',
+  templateUrl: './adicionar.component.html'
+})
+export class AdicionarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdicionarComponent } from './Model/Pessoa/adicionar/adicionar.component';
+import { ListarComponent } from './Model/Pessoa/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listar', component: ListarComponent},
+  {path: 'adicionar', component: AdicionarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
